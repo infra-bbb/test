1ed68d54580655b1175f791924248069c78a15f7
Mitsuya-bbb <bbb-squash@outlook.jp>
2020-09-16 12:06:49 +0900

    update test2.py test4.py

--- test2.py
+++ test2.py
@@ -2,3 +2,12 @@ print('this file was created by user1')
 
 user = 'user1'
 print(f'this message was written by {user}')
+
+'''
+error messages
+f
+f
+f
+f
+f
+'''
--- test4.py
+++ test4.py
@@ -1,1 +1,2 @@
 # this file was created by user4
+# this message was written by master
